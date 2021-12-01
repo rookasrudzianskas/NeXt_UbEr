39c9bd6c872c773c35dcf166086e5aa41f586e4d
Rokas <rookas.rudzenskas@gmail.com>
2021-12-01 16:49:56 +0200

    Rokas

--- pages/search.js
+++ pages/search.js
@@ -7,15 +7,16 @@ const Search = () => {
             <div className="bg-white px-4">
                 <Image src={'https://img.icons8.com/ios-filled/50/000000/left.png'} width={40} height={40} resizeMode={'contain'} className={"mt-2"}/>
             </div>
-            <div className="flex">
+            <div className="flex bg-white px-4">
                 <div className="flex flex-col">
-                    <Image src={'https://img.icons8.com/ios-filled/50/9CA3AF/filled-circle.png'} width={10} height={10} className={""}/>
+                    <Image src={'https://img.icons8.com/ios-filled/50/9CA3AF/filled-circle.png'} width={2} height={40} className={""}/>
                     <Image src={'https://img.icons8.com/ios/50/9CA3AF/vertical-line.png'} width={40} height={40} resizeMode={'contain'} className={""}/>
-                    <Image src={'https://img.icons8.com/windows/50/000000/square-full.png'} width={10} height={10} resizeMode={'contain'} className={""}/>
+                    <Image src={'https://img.icons8.com/windows/50/000000/square-full.png'} width={2} height={40} resizeMode={'contain'} className={""}/>
                 </div>
-            </div>
-            <div className="">
-
+                <div className="ml-5 flex flex-col">
+                    <input type="text" placeholder={'Search'} className={"h-10 bg-gray-200 outline-none"}/>
+                    <input type="text" placeholder={'Enter the destination location...'} className={"h-10 bg-gray-200 outline-none mt-8"}/>
+                </div>
             </div>
             <div className="">
 
