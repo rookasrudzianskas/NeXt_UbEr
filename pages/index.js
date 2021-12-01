ebf4582e794f45ba83be202d549071b1cef492c5
Rokas <rookas.rudzenskas@gmail.com>
2021-12-01 11:35:11 +0200

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import Head from 'next/head'
+import"mapbox-gl/dist/mapbox-gl.css";
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoicm9rYXNyIiwiYSI6ImNrc3VjdHM4YjBrdXcyb2xzaDlhNnJjbjMifQ.QptDnLGGkB4ffA97KWL6DA';
 
 export default function Home() {
+
   return (
-    <div className="flex flex-col min-h-screen px-4 bg-blue-500">
+    <div className="flex flex-col min-h-screen bg-blue-500">
       <Head>
         <title>UBER 🚚</title>
         <link rel="icon" href="/favicon.ico" />
