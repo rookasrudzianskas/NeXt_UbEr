0907df2baae865660951ec689f1c3a9e9493451d
Rokas <rookas.rudzenskas@gmail.com>
2021-12-01 13:48:03 +0200

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -17,9 +17,13 @@ export default function Home() {
 
 
       <Map />
-      <div className="bg-green-500 flex-1">
-          <div>
-              <Image src="https://i.ibb.co/84stgjq/uber-technologies-new-2021814.jpg" />
+      <div className="bg-white flex-1">
+          <div className="">
+              <Image className="h-28" src="https://i.ibb.co/84stgjq/uber-technologies-new-2021814.jpg" />
+              <div className="">
+                  <div className="">Rokas Rudzianskas</div>
+                  <Image className="h-28 w-28" src={'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg'} />
+              </div>
           </div>
       </div>
     </div>
