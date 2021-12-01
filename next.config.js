b1872cf10bb3aaff7b403890723e5b442fb34e37
Rokas <rookas.rudzenskas@gmail.com>
2021-12-01 14:52:31 +0200

    ROkas

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // next.config.js
 module.exports = {
     images: {
-        domains: ['assets.example.com', 'i.ibb.co', 'pbs.twimg.com'],
+        domains: ['assets.example.com', 'i.ibb.co', 'pbs.twimg.com', 'img.icons8.com'],
     },
 }
--- pages/search.js
+++ pages/search.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Image from "next/image";
 
 const Search = () => {
     return (
-        <div>
+        <div className="bg-gray-200 h-screen">
+            <div className="">
+                <Image src={'https://img.icons8.com/ios-filled/50/000000/left.png'} width={40} height={40} resizeMode={'contain'}/>
+            </div>
+            <div className="">
 
+            </div>
+            <div className="">
+
+            </div>
+            <div className="">
+
+            </div>
         </div>
     );
 };
