f5cae982f6a92ff110445302c3a25dbb1f8fe7c8
Rokas <rookas.rudzenskas@gmail.com>
2021-12-01 15:27:09 +0200

    Rokas

--- pages/search.js
+++ pages/search.js
@@ -7,8 +7,8 @@ const Search = () => {
             <div className="bg-white px-4">
                 <Image src={'https://img.icons8.com/ios-filled/50/000000/left.png'} width={40} height={40} resizeMode={'contain'} className={"mt-2"}/>
             </div>
-            <div className="">
-                <div>
+            <div className="flex">
+                <div className="flex flex-col">
                     <Image src={'https://img.icons8.com/ios-filled/50/9CA3AF/filled-circle.png'} width={10} height={10} resizeMode={'contain'} className={""}/>
                     <Image src={'https://img.icons8.com/ios/50/9CA3AF/vertical-line.png'} width={40} height={40} resizeMode={'contain'} className={""}/>
                     <Image src={'https://img.icons8.com/windows/50/000000/square-full.png'} width={10} height={10} resizeMode={'contain'} className={""}/>
