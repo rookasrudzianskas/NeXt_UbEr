f90c01544e1b393758e2acfa4293fe2cddf97101
Rokas <rookas.rudzenskas@gmail.com>
2021-12-01 11:49:23 +0200

    Rokas

--- components/Map.js
+++ components/Map.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import"mapbox-gl/dist/mapbox-gl.css";
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoicm9rYXNyIiwiYSI6ImNrc3VjdHM4YjBrdXcyb2xzaDlhNnJjbjMifQ.QptDnLGGkB4ffA97KWL6DA';
+
 
 const Map = () => {
-    return (
-        <div id={'map'}>
 
-        </div>
+
+    // const map = new mapboxgl.Map({
+    //     container: 'map',
+    //     style: 'mapbox://styles/rokasr/ckwnbxwet0log14t5m0zzuljv',
+    //     center: [-74.50, 40],
+    //     zoom: 9
+    // });
+
+    useEffect(() => {
+        // fires on first run 🔥
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/rokasr/ckwnbxwet0log14t5m0zzuljv',
+            center: [-74.50, 40],
+            zoom: 9
+        });
+    }, []);
+
+    return (
+        <div id={'map'} className="bg-red-500 flex-1"/>
+
     );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,10 @@
 import Head from 'next/head'
-import"mapbox-gl/dist/mapbox-gl.css";
-import mapboxgl from 'mapbox-gl';
 import {useEffect} from "react";
+import Map from "../components/Map";
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoicm9rYXNyIiwiYSI6ImNrc3VjdHM4YjBrdXcyb2xzaDlhNnJjbjMifQ.QptDnLGGkB4ffA97KWL6DA';
 
 export default function Home() {
 
-    // const map = new mapboxgl.Map({
-    //     container: 'map',
-    //     style: 'mapbox://styles/rokasr/ckwnbxwet0log14t5m0zzuljv',
-    //     center: [-74.50, 40],
-    //     zoom: 9
-    // });
-
-    useEffect(() => {
-        // fires on first run 🔥
-       const map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/rokasr/ckwnbxwet0log14t5m0zzuljv',
-            center: [-74.50, 40],
-            zoom: 9
-        });
-    }, []);
 
   return (
     <div className="flex flex-col min-h-screen bg-blue-500">
@@ -31,7 +13,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div id={'map'} className="bg-red-500 flex-1"></div>
+
+      <Map />
       <div className="bg-green-500 flex-1">Start</div>
     </div>
   )
