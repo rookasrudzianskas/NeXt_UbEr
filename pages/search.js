5fcb0ad012da69c5bba0a19f591c52c02bfa3a33
Rokas <rookas.rudzenskas@gmail.com>
2021-12-01 15:24:05 +0200

    Rokas

--- pages/search.js
+++ pages/search.js
@@ -8,7 +8,9 @@ const Search = () => {
                 <Image src={'https://img.icons8.com/ios-filled/50/000000/left.png'} width={40} height={40} resizeMode={'contain'} className={"mt-2"}/>
             </div>
             <div className="">
-
+                <div>
+                    <Image src={'https://img.icons8.com/ios-filled/50/9CA3AF/filled-circle.png'} width={40} height={40} resizeMode={'contain'} className={""}/>
+                </div>
             </div>
             <div className="">
 
