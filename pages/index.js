53e72d0eab93092f57dbd091413a91acd4f2aa25
Rokas <rookas.rudzenskas@gmail.com>
2021-12-01 11:38:05 +0200

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,23 @@
 import Head from 'next/head'
 import"mapbox-gl/dist/mapbox-gl.css";
 import mapboxgl from 'mapbox-gl';
+import {useEffect} from "react";
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoicm9rYXNyIiwiYSI6ImNrc3VjdHM4YjBrdXcyb2xzaDlhNnJjbjMifQ.QptDnLGGkB4ffA97KWL6DA';
 
 export default function Home() {
 
-    const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/rokasr/ckwnbxwet0log14t5m0zzuljv',
-        center: [-74.50, 40],
-        zoom: 9
-    });
+    // const map = new mapboxgl.Map({
+    //     container: 'map',
+    //     style: 'mapbox://styles/rokasr/ckwnbxwet0log14t5m0zzuljv',
+    //     center: [-74.50, 40],
+    //     zoom: 9
+    // });
+
+    useEffect(() => {
+        // fires on first run 🔥
+
+    }, []);
 
   return (
     <div className="flex flex-col min-h-screen bg-blue-500">
