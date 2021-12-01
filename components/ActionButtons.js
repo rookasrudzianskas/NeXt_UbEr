342546979afa297c2c9f01cb99fe6355908bac14
Rokas <rookas.rudzenskas@gmail.com>
2021-12-01 14:12:21 +0200

    Rokas

--- components/ActionButtons.js
+++ components/ActionButtons.js
@@ -4,15 +4,15 @@ import Image from "next/image";
 const ActionButtons = () => {
     return (
         <div className="flex flex-row px-4 items-center justify-center">
-            <div className="bg-gray-300 flex flex-1 items-center flex-col ">
+            <div className="bg-gray-300 flex flex-1 items-center flex-col  m-10 p-2 shadow-md hover:bg-gray-100 hover:shadow-lg  duration-200">
                 <Image src={'https://i.ibb.co/cyvcpfF/uberx.png'} height={'70'} width={'70'} />
                 Ride
             </div>
-            <div className="bg-gray-300 flex flex-1 items-center flex-col">
+            <div className="bg-gray-300 flex flex-1 items-center flex-col m-10 p-2 shadow-md hover:bg-gray-100 hover:shadow-lg  duration-200">
                 <Image src={'https://i.ibb.co/cyvcpfF/uberx.png'} height={'70'} width={'70'} />
                 2-Wheels
             </div>
-            <div className="bg-gray-300 flex flex-1 items-center flex-col">
+            <div className="bg-gray-300 flex flex-1 items-center flex-col m-10 p-2 shadow-md hover:bg-gray-100 hover:shadow-lg  duration-200">
                 <Image src={'https://i.ibb.co/cyvcpfF/uberx.png'} height={'70'} width={'70'} />
                 Reserve
             </div>
