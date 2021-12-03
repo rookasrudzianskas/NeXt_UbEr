4705d1d87b62fd24c9ba1d2372abb4db64bab673
Rokas <rookas.rudzenskas@gmail.com>
2021-12-03 09:03:28 +0200

    Rokas

--- pages/confirm.js
+++ pages/confirm.js
@@ -2,6 +2,17 @@ import React from 'react';
 import Map from "../components/Map";
 
 const confirm = () => {
+
+
+    const getCoorinates = () => {
+        const location = "Toms River"
+        // fetching the data
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=pk.eyJ1Ijoicm9rYXNyIiwiYSI6ImNrc3VjdHM4YjBrdXcyb2xzaDlhNnJjbjMifQ.QptDnLGGkB4ffA97KWL6DA`).then(response => response.json()).then(data => {
+            console.log(data)
+
+        })
+    }
+
     return (
         <div className="h-screen">
             <div className=" flex-1 h-96">
