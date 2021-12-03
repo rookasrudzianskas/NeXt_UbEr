00ecf87cca3fb576d84f2383fa87e0d37d37b3f6
Rokas <rookas.rudzenskas@gmail.com>
2021-12-03 15:59:59 +0200

    Rokas

---
+++ firebase.js
@@ -0,0 +1,21 @@
+
+import {initializeApp} from 'firebase/app';
+import firebase from "firebase/compat";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAURCaelDgrCgH5_3yR-9u3_rczxcVA43w",
+    authDomain: "rookas-uber.firebaseapp.com",
+    projectId: "rookas-uber",
+    storageBucket: "rookas-uber.appspot.com",
+    messagingSenderId: "313088053682",
+    appId: "1:313088053682:web:dea53f64cd95ddf0b5d4ab",
+    measurementId: "G-MP321C5LCW"
+};
+
+!firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
+const db = firebase.firestore();
+
+export {firebase, db};
+
--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Image from "next/image";
 
 const Login = () => {
     return (
-        <div className="">
-
+        <div className="flex items-center justify-center h-screen bg-gray-200 flex-col">
+            <Image src={'https://i.ibb.co/n6LWQM4/Post.png'} width="800" height="300" resizeMode={'contain'}/>
+            <div className="text-white px-10 py-5 rounded-md font-bold bg-black hover:cursor-pointer mt-10">Sign in with Google</div>
         </div>
     );
 };
