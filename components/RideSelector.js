66c1946c5b77000727661400d687e95cf65da33e
Rokas <rookas.rudzenskas@gmail.com>
2021-12-03 14:36:22 +0200

    Rokas

--- components/RideSelector.js
+++ components/RideSelector.js
@@ -3,8 +3,8 @@ import React from 'react';
 const RideSelector = () => {
     return (
         <div className="">
-            <div className="text-gray-500 flex text-center">
-                <span className="text-center ml-10 mt-4">Choose a ride, or swipe to the bottom m to create a new ride.</span>
+            <div className="text-gray-500 flex text-center border-b w-full">
+                <span className="text-center ml-10 mt-4 mr-10">Choose a ride, or swipe to the bottom m to create a new ride.</span>
             </div>
 
             <div className="">
